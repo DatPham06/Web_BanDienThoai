37c0c01459142ef81eb867bb81f218f87ccfbb0c
DatPham06 <pdat1969@gmail.com>
2025-10-23 17:59:06 +0700

    gan banner cho trang chu

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,10 +8,6 @@ namespace WebBanHang.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
         public ActionResult TrangChu()
         {
             return View();
